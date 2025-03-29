151d8f78f770f558e0ed57eb4bdd0cbf5dcac1b8
Lucas Vilela <lucasvilelapessoal@gmail.com>
2025-03-29 17:26:11 -0300

    Commit spring initializer

---
+++ labdev-spring/src/main/java/com/labdev/labdev_spring/LabdevSpringApplication.java
@@ -0,0 +1,13 @@
+package com.labdev.labdev_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabdevSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabdevSpringApplication.class, args);
+	}
+
+}
---
+++ labdev-spring/src/test/java/com/labdev/labdev_spring/LabdevSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labdev.labdev_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabdevSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

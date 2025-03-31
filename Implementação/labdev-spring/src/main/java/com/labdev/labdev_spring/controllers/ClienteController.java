c80703551df9aab787d77f7f4fead31dd0c3eaeb
Lucas Flor Vilela <lucasvilelapessoal@gmail.com>
2025-03-31 11:00:53 -0300

    commit tirando o inutilizado

--- Implementação/labdev-spring/src/main/java/com/labdev/labdev_spring/controllers/ClienteController.java
+++ Implementação/labdev-spring/src/main/java/com/labdev/labdev_spring/controllers/ClienteController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import com.labdev.labdev_spring.models.Cliente;
 import com.labdev.labdev_spring.repositories.ClienteRepository;
-
-import java.util.Arrays;
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -28,7 +24,7 @@ public class ClienteController {
   List<Cliente> clientes = this.clienteRepository.findAll();
 
   ModelAndView mv = new ModelAndView("clientes/index");
-  mv.addObject("clientes", clientes); // ✅ Corrigido para "clientes" (tudo minúsculo)
+  mv.addObject("clientes", clientes);
 
   return mv;
  }
